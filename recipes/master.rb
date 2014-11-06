3bb3e91bb3465f2f1dd693e77f61814f6f8932c4
Lopaka Delp <lopaka@rightscale.com>
2014-11-06 00:58:48 -0800

    Update testing.

--- recipes/master.rb
+++ recipes/master.rb
@@ -10,8 +10,8 @@ include_recipe 'rs-mysql::master'
 
 dns_name, domain_name = node['rs-mysql']['dns']['master_fqdn'].split('.', 2)
 
-rewind 'dns[dns_name]' do
-  provider 'default'
+rewind :dns => dns_name do
+  provider Chef::Provider::Dns
   dns_provider 'aws'
   credentials(
     'aws_acesss_key_id' => node['rs-mysql']['dns']['user_key'],
